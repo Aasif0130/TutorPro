07f55f4f9c92d1bfdda87ebc925d9e2494fa2592
Mohamad Aasif <aasif@Mohamads-MacBook-Pro.local>
2026-05-09 14:18:00 +0530

    Fix Streamlit Cloud secrets KeyError

--- auth.py
+++ auth.py
@@ -5,7 +5,12 @@ def check_password():
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if st.session_state["password"] == st.secrets["passwords"]["teacher_password"]:
+        try:
+            expected_password = st.secrets["passwords"]["teacher_password"]
+        except (KeyError, FileNotFoundError):
+            expected_password = "admin"
+            
+        if st.session_state["password"] == expected_password:
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # don't store password
         else:
